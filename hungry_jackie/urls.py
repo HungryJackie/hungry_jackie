840829934ebe3656b2a24843c3a8a640e2accdd6
Sususu <iamsummer614@gmail.com>
2025-08-24 18:40:39 +0900

    social login.html 구현

--- hungry_jackie/urls.py
+++ hungry_jackie/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from accounts import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
+    path('', views.home, name='home'),  # 메인 페이지를 위한 URL을 추가합니다.
 ]
